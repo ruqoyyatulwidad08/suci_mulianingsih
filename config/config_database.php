2dd7cab6d932e3917e6c977e0837bde361a001ec
ruqoyyatulwidad08 <138642385+ruqoyyatulwidad08@users.noreply.github.com>
2023-08-03 17:47:58 +0700

    suci

---
+++ config/config_database.php
@@ -0,0 +1,8 @@
+<?php
+require "config-global.php";
+fucntion($host, $nama, $alamat,$db) {
+    koneksi(hostname, alamat, password, database)
+    $koneksi = new mysql($host, $user, $password, $db);
+    return $koneksi;
+}
+
---
+++ config/config_global.php
@@ -0,0 +1,5 @@
+<?php
+const hostnime = 'localhost'
+ const username = 'root';
+ const password = '';
+ const database = 'mydatabase';
